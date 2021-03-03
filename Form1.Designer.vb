654d649cb2360168363506fe93370f594450a76f
sketchware69 <76958921+sketchware69@users.noreply.github.com>
2021-03-03 14:08:19 -0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.response = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.PlaceholderText = "url to request"
+        Me.TextBox1.Size = New System.Drawing.Size(507, 67)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 73)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(507, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "request"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'response
+        '
+        Me.response.AutoSize = True
+        Me.response.Location = New System.Drawing.Point(12, 99)
+        Me.response.Name = "response"
+        Me.response.Size = New System.Drawing.Size(138, 15)
+        Me.response.TabIndex = 2
+        Me.response.Text = "response should be here!"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 117)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(42, 19)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "get"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 142)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(48, 19)
+        Me.RadioButton2.TabIndex = 4
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "post"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(528, 336)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.response)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(544, 375)
+        Me.MinimumSize = New System.Drawing.Size(544, 375)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents response As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+
+Public Class Form1
+    Dim logincookie As CookieContainer
+    Dim rqtp As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim postReq As HttpWebRequest = DirectCast(WebRequest.Create(TextBox1.Text), HttpWebRequest)
+        Select Case rqtp
+            Case 1
+                Dim postData As String = TextBox1.Text
+                Dim encoding As New UTF8Encoding
+                Dim byteData As Byte() = encoding.GetBytes(postData)
+                Dim tempCookies As New CookieContainer
+                postReq.Method = "GET"
+                postReq.KeepAlive = True
+                postReq.CookieContainer = tempCookies
+                postReq.ContentType = "application/xml"
+                postReq.Referer = TextBox1.Text
+                postReq.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)"
+                postReq.ContentLength = byteData.Length
+                Dim postreqstream As Stream = postReq.GetRequestStream()
+                postreqstream.Write(byteData, 0, byteData.Length)
+                postreqstream.Close()
+                Dim postresponse As HttpWebResponse
+
+                postresponse = DirectCast(postReq.GetResponse(), HttpWebResponse)
+                tempCookies.Add(postresponse.Cookies)
+                logincookie = tempCookies
+                Dim postreqreader As New StreamReader(postresponse.GetResponseStream())
+
+                Dim thepage As String = postreqreader.ReadToEnd
+
+                response.Text = thepage
+                Clipboard.Clear()
+                Clipboard.SetText(thepage)
+                MsgBox(thepage, vbInformation + vbOK, "result")
+
+
+
+
+                REM Dim webClient As New System.Net.WebClient
+                REM Dim result As String = webClient.DownloadString(TextBox1.Text)
+                REM If Not result = "" Then
+                REM    Clipboard.Clear()
+                REM    Clipboard.SetText(result)
+                REM MsgBox(result, vbInformation + vbOK, "result")
+                REM    response.Text = (result)
+                REM End If
+            Case 2
+                Dim postData As String = TextBox1.Text
+                Dim tempCookies As New CookieContainer
+                Dim encoding As New UTF8Encoding
+                Dim byteData As Byte() = encoding.GetBytes(postData)
+                postReq.Method = "POST"
+                postReq.KeepAlive = True
+                postReq.CookieContainer = tempCookies
+                postReq.ContentType = "application/xml"
+                postReq.Referer = TextBox1.Text
+                postReq.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)"
+                postReq.ContentLength = byteData.Length
+
+                Dim postreqstream As Stream = postReq.GetRequestStream()
+                postreqstream.Write(byteData, 0, byteData.Length)
+                postreqstream.Close()
+                Dim postresponse As HttpWebResponse
+
+                postresponse = DirectCast(postReq.GetResponse(), HttpWebResponse)
+                tempCookies.Add(postresponse.Cookies)
+                logincookie = tempCookies
+                Dim postreqreader As New StreamReader(postresponse.GetResponseStream())
+
+                Dim thepage As String = postreqreader.ReadToEnd
+
+                response.Text = thepage
+                Clipboard.Clear()
+                Clipboard.SetText(thepage)
+                MsgBox(thepage, vbInformation + vbOK, "result")
+        End Select
+    End Sub
+
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        rqtp = 1
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        rqtp = 2
+    End Sub
+End Class
